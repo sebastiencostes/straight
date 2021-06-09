795195194e0b391e86c610d63b8eb723d6782f8e
Sébastien Costes <sebastiencostes@airdesebastien.home>
2021-06-10 00:28:13 +0200

    refactor js carousel card, pages

--- script/carousel.js
+++ script/carousel.js
@@ -1,28 +1,57 @@
-var slideIndex = 1;
+const slides = document.querySelectorAll(".carousel-slide");
+const btnCarousel = document.querySelectorAll(".carousel-cta");
+const next = document.querySelector(".carousel-next");
+const prev = document.querySelector(".carousel-prev");
+
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex += n));
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  let i;
+  let slides = document.getElementsByClassName("carousel-slide");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
- 
+  slides[slideIndex - 1].style.display = "block";
 }
+
+
+
+
+// prev.addEventListener("click", prevSlide);
+// next.addEventListener("click", nextSlide);
+// slides[slideIndex].style.display = "block";
+// function prevSlide(n) {
+//   if(slideIndex < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for(let i = 0; i < slides.length; i++) {
+    
+//     slides[i].style.display = "none";
+//     console.log(slideIndex);
+//   } 
+//   slides[slideIndex - 1].style.display = "block";
+// }
+
+// function nextSlide() {
+//   if(slideIndex > slides.length) {
+//     slideIndex = 1;
+//   }
+//   for(let i = 0; i < slides.length; i++) {
+    
+//     slides[i].style.display = "none";
+//     console.log(slideIndex);
+//   }
+//   slides[slideIndex + 1].style.display = "block";
+// }
