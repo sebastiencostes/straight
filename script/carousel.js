464105652d078ffc3fca498c74b86870b7d7b9aa
Sébastien Costes <sebastiencostes@airdesebastien.home>
2021-06-11 00:33:34 +0200

    wip card and carousel

--- script/carousel.js
+++ script/carousel.js
@@ -1,57 +1,77 @@
+const carousels = document.querySelectorAll(".carousel");
 const slides = document.querySelectorAll(".carousel-slide");
-const btnCarousel = document.querySelectorAll(".carousel-cta");
 const next = document.querySelector(".carousel-next");
 const prev = document.querySelector(".carousel-prev");
+const buttonsPrev = document.querySelectorAll(".carousel-prev");
+const buttonsNext = document.querySelectorAll(".carousel-next");
+const error = 'Straight Carousel - Simon says : ‘Please add an ID to <article id="" class="carousel" />‘'
 
 let slideIndex = 1;
-showSlides(slideIndex);
+carousel(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+// prev.addEventListener("click", () => {
+//   // for(let slide of slides) {
+//   //   slide.style.setProperty('--animate-duration', '0.28s');
+//   //   slide.classList.remove('animate__animated', 'animate__slideInLeft');
+//   //   slide.classList.add('animate__animated', 'animate__slideInRight');
+//   // }
+//   carousel((slideIndex = slideIndex - 1));
+// });
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("carousel-slide");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
+// next.addEventListener("click", () => {
+//   // for(let slide of slides) {
+//   //   slide.style.setProperty('--animate-duration', '0.28s');
+//   //   slide.classList.remove('animate__animated', 'animate__slideInRight');
+//   //   slide.classList.add('animate__animated', 'animate__slideInLeft');
+//   // }
+//   carousel((slideIndex = slideIndex + 1));
+// });
+
+function carousel(tmpSlideIndex) {
+  if (tmpSlideIndex < 1) {
     slideIndex = slides.length;
   }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  if (tmpSlideIndex > slides.length) {
+    slideIndex = 1;
   }
+  for (let slide of slides) {
+    if (
+      prev.getAttribute("data-carousel") ===
+        slide.getAttribute("data-carousel") &&
+      next.getAttribute("data-carousel") === slide.getAttribute("data-carousel")
+    ) {
+      slide.style.display = "none";
+    }
+  }
+  
   slides[slideIndex - 1].style.display = "block";
 }
 
+///////////
+////1/////
+//////////
+let carouselsId = "";
+let newCarousels = document.getElementById(carouselsId);
 
 
+prev.addEventListener("click", (e) => {
+  carouselsId = e.currentTarget.parentNode.id;
+  for(let slide of slides) {
+    slide.setAttribute('straigth-carousel', carouselsId);
+    let isId = slide.getAttribute('straigth-carousel') === carouselsId;
+    isId ? carousel((slideIndex = slideIndex + 1)) : console.log(error);
+  }
+  carousel((slideIndex = slideIndex - 1));
+  console.log(carouselsId);
+});
 
-// prev.addEventListener("click", prevSlide);
-// next.addEventListener("click", nextSlide);
-// slides[slideIndex].style.display = "block";
-// function prevSlide(n) {
-//   if(slideIndex < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for(let i = 0; i < slides.length; i++) {
-    
-//     slides[i].style.display = "none";
-//     console.log(slideIndex);
-//   } 
-//   slides[slideIndex - 1].style.display = "block";
-// }
-
-// function nextSlide() {
-//   if(slideIndex > slides.length) {
-//     slideIndex = 1;
-//   }
-//   for(let i = 0; i < slides.length; i++) {
-    
-//     slides[i].style.display = "none";
-//     console.log(slideIndex);
-//   }
-//   slides[slideIndex + 1].style.display = "block";
-// }
+next.addEventListener("click", (e) => {
+  carouselsId = e.currentTarget.parentNode.id;
+  for(let slide of slides) {
+    slide.setAttribute('straigth-carousel', carouselsId);
+    let isId = slide.getAttribute('straigth-carousel') === carouselsId;
+    isId ? carousel((slideIndex = slideIndex + 1)) : console.log(error);
+  }
+  carousel((slideIndex = slideIndex + 1));
+  console.log(carouselsId);
+});
