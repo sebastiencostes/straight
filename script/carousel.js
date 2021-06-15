91bb558187c8bd037c293092133cc726181f7d6e
Sébastien Costes <sebastiencostes@airdesebastien.home>
2021-06-15 15:25:04 +0200

    wip card, carousel

--- script/carousel.js
+++ script/carousel.js
@@ -1,94 +1,41 @@
-let carouselsId = "";
-let ids = [];
-let classes = []
-let slideIndex = 1;
-const carousels = document.querySelectorAll(".carousel");
-const slides = document.querySelectorAll(".carousel-slide");
-const next = document.querySelector(".carousel-next");
-const prev = document.querySelector(".carousel-prev");
-const buttonsPrev = document.querySelectorAll(".carousel-prev");
-const buttonsNext = document.querySelectorAll(".carousel-next");
-const error =
-  'Straight Carousel - Simon says : ‘Please add an ID to <article id="" class="carousel" />‘';
+const controls = document.querySelectorAll('.carousel-cta')
+let  slideIndex = 1
+//var slideIndex = Array.prototype.indexOf.call(parent.children, child);
 
+carousel = (id) => {
+  const carouselID = document.querySelector(id)
+  const sliders = carouselID.querySelectorAll('.carousel-slide')
 
-
-  for(carousel of carousels) {
-    let tmpId = carousel.getAttribute("id");
-    ids.push(tmpId);
-      for (let slide of slides) {
-        let tmpClass = tmpId+ '  ' +slide.getAttribute("class");
-        console.log(tmpClass)
-      }
-      
+  if (slideIndex < 1) {
+    slideIndex = sliders.length
   }
-  console.log(ids.includes('carousel'));
-////ids
+  if (slideIndex > sliders.length) {
+    slideIndex = 1
+  }
+
+  for (let slide of sliders) {
+    slide.classList.remove('d-block')
+    slide.classList.add('d-none')
+  }
   
-
-
-
-
-
-
-straigthCarousel(slideIndex);
-
-
-// prev.addEventListener("click", () => {
-//   // for(let slide of slides) {
-//   //   slide.style.setProperty('--animate-duration', '0.28s');
-//   //   slide.classList.remove('animate__animated', 'animate__slideInLeft');
-//   //   slide.classList.add('animate__animated', 'animate__slideInRight');
-//   // }
-//   carousel((slideIndex = slideIndex - 1));
-// });
-
-// next.addEventListener("click", () => {
-//   // for(let slide of slides) {
-//   //   slide.style.setProperty('--animate-duration', '0.28s');
-//   //   slide.classList.remove('animate__animated', 'animate__slideInRight');
-//   //   slide.classList.add('animate__animated', 'animate__slideInLeft');
-//   // }
-//   carousel((slideIndex = slideIndex + 1));
-// });
-
-for(let btn of buttonsPrev) {
-  btn.addEventListener("click", (e) => {
-  carouselsId = e.currentTarget.parentNode.id;
-  for (let slide of slides) {
-    slide.setAttribute("straigth-carousel", carouselsId);
-    // let isId = slide.getAttribute("straigth-carousel") === carouselsId;
-    // isId ? carousel((slideIndex = slideIndex - 1)) : console.log(error);
-  }
-  straigthCarousel((slideIndex = slideIndex - 1));
-  console.log(carouselsId);
-});
+  sliders[slideIndex -1].classList.remove('d-none')
+  sliders[slideIndex -1].classList.add('d-block')
+  console.log('slideIndex dans carousel: ' + slideIndex)
 }
 
-for(let btn of buttonsNext) {
-  btn.addEventListener("click", (e) => {
-  carouselsId = e.currentTarget.parentNode.id;
-  for (let slide of slides) {
-    slide.setAttribute("straigth-carousel", carouselsId);
-    // let isId = slide.getAttribute("straigth-carousel") === carouselsId;
-    // isId ? carousel((slideIndex = slideIndex + 1)) : console.log(error);
-  }
-  straigthCarousel((slideIndex = slideIndex + 1));
-  console.log(carouselsId);
-});
+for(let btn of controls) {
+  btn.addEventListener('click', (e) => {
+    //fetch current carousel id
+    let tmpId = '#' + e.currentTarget.parentNode.id
+
+    //check if cta is prev or next and go to to next or prev slide
+    let isPrev = e.currentTarget.classList.contains('carousel-prev')
+    isPrev ? (carousel(tmpId), slideIndex--) : (carousel(tmpId), slideIndex++)
+
+    console.log('slideIndex dans button : ' + slideIndex)
+  })
 }
 
-
-function straigthCarousel(tmpSlideIndex) {
-  if (tmpSlideIndex < 1) {
-    slideIndex = slides.length;
-  }
-  if (tmpSlideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  for (let slide of slides) {
-    slide.style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
-
+carousel('#carousel1')
+carousel('#carousel2')
+carousel('#carousel3')
