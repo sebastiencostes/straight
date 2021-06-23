3ce14ae83e75431139e28405930239a6c685d505
Sébastien Costes <sebastiencostes@airdesebastien.home>
2021-06-24 01:29:31 +0200

    wip block code and clipboard

--- script/card.js
+++ script/card.js
@@ -76,36 +76,3 @@ for (let button of btnClose) {
     }
   });
 }
-
-const aioColors = document.querySelectorAll('.clipboard');
-
-aioColors.forEach(color => {
-  color.addEventListener('click', () => {
-    const selection = window.getSelection();
-    const range = document.createRange();
-    range.selectNodeContents(color);
-    selection.removeAllRanges();
-    selection.addRange(range);
-
-    try {
-      document.execCommand('copy');
-      selection.removeAllRanges();
-
-      const original = color.textContent;
-      color.textContent = 'Copied!';
-      color.classList.add('success');
-
-      setTimeout(() => {
-        color.textContent = original;
-        color.classList.remove('success');
-      }, 1200);
-    } catch(e) {
-      const errorMsg = document.querySelector('.error-msg');
-      errorMsg.classList.add('show');
-
-      setTimeout(() => {
-        errorMsg.classList.remove('show');
-      }, 1200);
-    }
-  });
-});
