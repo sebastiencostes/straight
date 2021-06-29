02116a84e374176c3826002845ae80cd4f5b35e0
Sébastien Costes <sebastiencostes@airdesebastien.home>
2021-06-30 00:53:42 +0200

    wip toggling nav

--- script/nav.js
+++ script/nav.js
@@ -1,198 +1,214 @@
-//consts for classic nav
-const navs = document.querySelectorAll(".nav");
-const menus = document.querySelectorAll(".nav-menu");
+const arrayStraightNav = [
+  {
+    link: "/button.html",
+    linkName: "button",
+    icon: "ellipsis-horizontal-outline",
+    color: "purple",
+  },
+  {
+    link: "/card.html",
+    linkName: "card",
+    icon: "newspaper-outline",
+    color: "pink",
+  },
+  {
+    link: "/carousel.html",
+    linkName: "carousel",
+    icon: "code-outline",
+    color: "orange",
+  },
+  {
+    link: "/chart.html",
+    linkName: "chart",
+    icon: "stats-chart-outline",
+    color: "turquoise",
+  },
+  {
+    link: "/color.html",
+    linkName: "color",
+    icon: "color-palette-outline",
+    color: "black",
+  },
+  { link: "/grid.html", linkName: "grid", icon: "grid-outline", color: "navy" },
+  {
+    link: "/header.html",
+    linkName: "header",
+    icon: "reader-outline",
+    color: "yellow",
+  },
+  {
+    link: "/image.html",
+    linkName: "image",
+    icon: "image-outline",
+    color: "green",
+  },
+  {
+    link: "/modal.html",
+    linkName: "modal",
+    icon: "albums-outline",
+    color: "white",
+  },
+  {
+    link: "/notification.html",
+    linkName: "notification",
+    icon: "notifications-outline",
+    color: "red",
+  },
+  {
+    link: "/typography.html",
+    linkName: "typography",
+    icon: "text-outline",
+    color: "brown",
+  },
+  {
+    link: "/nav.html",
+    linkName: "nav",
+    icon: "navigate-outline",
+    color: "blue",
+  },
+  {
+    link: "/utility.html",
+    linkName: "utility",
+    icon: "hammer-outline",
+    color: "grey",
+  },
+];
+const switchNav = document.querySelector(".switch-nav");
+const menu = document.querySelector(".nav-menu");
 const menuItems = document.querySelectorAll(".nav-menu-item");
-const openBtn = document.querySelectorAll(".nav .open");
-const closeBtn = document.querySelectorAll(".nav .close");
-//consts for nav pad
-const menusPad = document.querySelectorAll(".nav-pad-menu");
-const menuItemsPad = document.querySelectorAll(".navpad--menu-item");
-const openBtnPad = document.querySelectorAll(".nav-pad .open");
-const closeBtnPad = document.querySelectorAll(".nav-pad .close");
-
+const openBtn = document.querySelector(".nav .open");
+const closeBtn = document.querySelector(".nav .close");
+const gridBtn = document.querySelector(".nav .grid");
+const listBtn = document.querySelector(".nav .list");
 /*
-Open inner nav
-Nav used for straight frmaework demo
+NAV
 */
-straightNav = () => {
-  const arrayStraightNav = [
-    { link: "/button.html", linkName: "button" },
-    { link: "/card.html", linkName: "card" },
-    { link: "/carousel.html", linkName: "carousel" },
-    { link: "/chart.html", linkName: "chart" },
-    { link: "/color.html", linkName: "color" },
-    { link: "/header.html", linkName: "header" },
-    { link: "/modal.html", linkName: "modal" },
-    { link: "/notification.html", linkName: "notification" },
-    { link: "/typography.html", linkName: "typography" },
-    { link: "/nav.html", linkName: "nav" },
-  ];
-  const straightNavClassic = document.querySelector("#straightNav");
-  const straightMenu = straightNavClassic.querySelector(".nav-menu");
+nav = () => {
+  let isList = "";
+  let isOpen = "";
+
+  //generating list
   for (let url of arrayStraightNav) {
-    straightMenu.innerHTML += `<li class="nav-menu-item" role="menuitem">
-    <a class="nav-menu-link" href="${url.link}" rel="noopener">
-        ${url.linkName}
+    switchNav.innerHTML += `<li class="nav-menu-item" role="menuitem">
+    <a class="nav-menu-link btn-square-m btn-${url.color}" href="${url.link}" rel="noopener" role="button">
+    <ion-icon name="${url.icon}"></ion-icon>
+    </a>
+    <a class="nav-menu-link-text" href="${url.link}" rel="noopener">
+    ${url.linkName}
     </a>
     </li>`;
   }
-};
-straightNav();
+
 /*
-Close inner nav
+OPEN MENU
 */
+  openMenu = (open, list) => {
+    isOpen = open;
+    isList = list;
+    gridBtn.classList.remove("d-none");
+    gridBtn.classList.add("d-block");
+    isOpen ? null : closeBtn.classList.remove("d-none"),
+      closeBtn.classList.add("d-block");
+    isList
+      ? (switchNav.classList.add("nav-menu"),
+        switchNav.classList.remove("d-none"),
+        switchNav.classList.add("d-block"))
+      : null;
+  };
+  openBtn.addEventListener("click", (e) => {
+    openMenu(true, true);
+    e.currentTarget.classList.add("d-none");
+    e.currentTarget.classList.remove("d-block");
+  });
 
-classicNav = () => {
-  //fetch and set all data-attr on appropriate elements
-  for (let i = 0; i < navs.length; i++) {
-    navs[i].setAttribute("data-nav", i);
-  }
-  for (let i = 0; i < openBtn.length; i++) {
-    openBtn[i].setAttribute("data-nav", i);
-  }
-  for (let i = 0; i < closeBtn.length; i++) {
-    closeBtn[i].setAttribute("data-nav", i);
-  }
-  for (let i = 0; i < menus.length; i++) {
-    menus[i].setAttribute("data-nav", i);
-  }
+/*
+CLOSE MENU
+*/
+  closeMenu = (close, list) => {
+    isOpen = close;
+    isList = list;
+    gridBtn.classList.remove("d-block");
+    gridBtn.classList.add("d-none");
+    isOpen ? null : openBtn.classList.remove("d-none"),
+      openBtn.classList.add("d-block");
+    isList
+      ? (switchNav.classList.remove("nav-menu"),
+        switchNav.classList.add("d-none"),
+        switchNav.classList.remove("d-block"))
+      : null;
+  };
+  closeBtn.addEventListener("click", (e) => {
+    closeMenu(false, true);
+    e.currentTarget.classList.add("d-none");
+    e.currentTarget.classList.remove("d-block");
+  });
 
-  //show menu
-  for (let btn of openBtn) {
-    btn.addEventListener("click", (e) => {
-      for (let menu of menus) {
-        if (menu.getAttribute("data-nav") === e.currentTarget.getAttribute("data-nav")) {
-          e.currentTarget.classList.add("d-none");
-          e.currentTarget.classList.remove("d-block");
-          for (let btn of closeBtn) {
-            btn.classList.remove("d-none");
-            btn.classList.add("d-block");
-          }
-          menu.classList.remove("d-none");
-          menu.classList.add("d-block");
-        }
-      }
-    });
-  }
+/*
+GRID VIEW DISPLAYING
+*/
+  gridView = (list) => {
+    isList = list;
+    listBtn.classList.add("d-block");
+    listBtn.classList.remove("d-none");
 
-  //hide menu
-  for (let btn of closeBtn) {
-    btn.addEventListener("click", (e) => {
-      for (let menu of menus) {
-        if (menu.getAttribute("data-nav") === e.currentTarget.getAttribute("data-nav")) {
-          e.currentTarget.classList.add("d-none");
-          e.currentTarget.classList.remove("d-block");
-          for (let btn of openBtn) {
-            btn.classList.remove("d-none");
-            btn.classList.add("d-block");
-          }
-          menu.classList.remove("d-block");
-          menu.classList.add("d-none");
-        }
-      }
-    });
-  }
+    isList
+      ? null
+      : (switchNav.classList.remove("nav-menu", "d-block"),
+        switchNav.classList.add(
+          "nav-pad-menu",
+          "d-grid",
+          "g-col-s-2",
+          "g-col-m-3",
+          "g-col-l-4",
+          "g-col-xl-5",
+          "g-col-xxl-6"
+        ));
+  };
+  gridBtn.addEventListener("click", (e) => {
+    gridView(false);
+    e.currentTarget.classList.remove("d-block");
+    e.currentTarget.classList.add("d-none");
+  });
+
+/*
+LIST VIEW DISPLAYING
+*/
+  listView = (list) => {
+    isList = list;
+    gridBtn.classList.add("d-block");
+    gridBtn.classList.remove("d-none");
+
+    isList
+      ? null
+      : (switchNav.classList.add("nav-menu", "d-block"),
+        switchNav.classList.remove(
+          "nav-pad-menu",
+          "d-grid",
+          "g-col-s-2",
+          "g-col-m-3",
+          "g-col-l-4",
+          "g-col-xl-5",
+          "g-col-xxl-6"
+        ));
+  };
+  listBtn.addEventListener("click", (e) => {
+    listView(false);
+    e.currentTarget.classList.remove("d-block");
+    e.currentTarget.classList.add("d-none");
+  });
+
   //hide menu when clicking on li
   for (let item of menuItems) {
     item.addEventListener("click", () => {
-      for (let menu of menus) {
-        menu.classList.remove("d-block");
-        menu.classList.add("d-none");
-      }
-      for (let btn of closeBtn) {
-        btn.classList.add("d-none");
-        btn.classList.remove("d-block");
-      }
-      for (let btn of openBtn) {
-        btn.classList.remove("d-none");
-        btn.classList.add("d-block");
-      }
+      switchNav.classList.remove("d-block");
+      switchNav.classList.add("d-none");
+
+      closeBtn.classList.add("d-none");
+      closeBtn.classList.remove("d-block");
+
+      openBtn.classList.remove("d-none");
+      openBtn.classList.add("d-block");
     });
   }
 };
-classicNav();
-
-/*
-Open navPad
-*/
-navPad = () => {
-  //fetch and set all data-attr on appropriate elements
-  for (let i = 0; i < openBtnPad.length; i++) {
-    openBtnPad[i].setAttribute("data-nav", i);
-  }
-  for (let i = 0; i < menusPad.length; i++) {
-    menusPad[i].setAttribute("data-nav", i);
-  }
-  for (let i = 0; i < closeBtnPad.length; i++) {
-    closeBtnPad[i].setAttribute("data-nav", i);
-  }
-  //show menu
-  for (let btn of openBtnPad) {
-    btn.addEventListener("click", (e) => {
-      for (let menu of menusPad) {
-        if (menu.getAttribute("data-nav") === btn.getAttribute("data-nav")) {
-          e.currentTarget.classList.add("d-none");
-          e.currentTarget.classList.remove("d-block");
-          for (let btn of closeBtnPad) {
-            btn.classList.remove("d-none");
-            btn.classList.add("d-block");
-          }
-          menu.classList.add("d-block");
-        }
-      }
-    });
-  }
-
-  //hide menu
-  for (let btn of closeBtnPad) {
-    btn.addEventListener("click", (e) => {
-      for (let menu of menusPad) {
-        if (menu.getAttribute("data-nav") === btn.getAttribute("data-nav")) {
-          e.currentTarget.classList.add("d-none");
-          e.currentTarget.classList.remove("d-block");
-          for (let btn of openBtnPad) {
-            btn.classList.remove("d-none");
-            btn.classList.add("d-block");
-          }
-          menu.classList.remove("d-block");
-        }
-      }
-    });
-  }
-  //hide menu when clicking on li
-  for (let item of menuItemsPad) {
-    item.addEventListener("click", () => {
-      menuPad.classList.remove("d-block");
-      menuPad.classList.add("d-none");
-      closeBtnPad.classList.add("d-none");
-      closeBtnPad.classList.remove("d-block");
-      openBtnPad.classList.remove("d-none");
-      openBtnPad.classList.add("d-block");
-    });
-  }
-};
-navPad();
-
-// let idParent = e.currentTarget.parentNode.parentNode.id;
-// console.log(idParent);
-
-// for (let item of menuItems) {
-//     item.addEventListener("click", (e) => {
-//       for (let menu of menus) {
-//        // if (menu.getAttribute("data-nav") === item.getAttribute("data-nav")) {
-//             menu.classList.remove("d-block");
-//             menu.classList.add("d-none");
-//             e.currentTarget.classList.add("d-none");
-//             e.currentTarget.classList.remove("d-block");
-//           for (let btn of openBtn) {
-//             btn.classList.remove("d-none");
-//             btn.classList.add("d-block");
-//           }
-//           for (let btn of closeBtn) {
-//             btn.classList.add("d-none");
-//             btn.classList.remove("d-block");
-//           }
-//         //}
-//       }
-//     });
-//   }
+nav();
