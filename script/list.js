cbd74a35a7b82a350ae3ee4949cd1039573f8547
Sébastien Costes <sebastiencostes@airdesebastien.home>
2021-07-19 17:23:11 +0200

    done 1st iteration list

--- script/list.js
+++ script/list.js
@@ -1,18 +1,23 @@
 const accordionContainer = document.querySelectorAll(".accordion-container");
-const listContainer = document.querySelectorAll(".list-container");
+const listContainer = document.querySelectorAll(".accordion-container .list-container");
 const accordionCta = document.querySelectorAll(".accordion-cta");
 
 //set attr for each triiger and list container
-for(let i = 0;  i < listContainer.length; i++) {
-    listContainer[i].setAttribute('data-accordion', i);
-    accordionCta[i].setAttribute('data-accordion', i);
+for (let i = 0; i < listContainer.length; i++) {
+  listContainer[i].setAttribute("data-accordion", i);
+  accordionCta[i].setAttribute("data-accordion", i);
 }
 
 //show hide list
 toggleList = (accordion, trigger) => {
-    if(accordion.getAttribute("data-accordion") === trigger.getAttribute("data-accordion")) {
-        accordion.classList.toggle("d-none");
-    }
+  if (
+    accordion.getAttribute("data-accordion") ===
+    trigger.getAttribute("data-accordion")
+  ) {
+    //accordion.classList.toggle("d-none");
+    accordion.classList.toggle("accordion-open");
+    accordion.classList.toggle("accordion-close");
+  }
 };
 
 //call toggleList()
